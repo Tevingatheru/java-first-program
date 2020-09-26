789eee0d6cec21dde9416d79bab0d877f664e63c
unknown <gatheruteving@gmail.com>
2020-09-27 00:21:52 +0300

    move package

---
+++ src/main/java/Finance.java
@@ -0,0 +1,55 @@
+import com.h2.BestLoanRates;
+import com.h2.MortgageCalculator;
+import com.h2.SavingsCalculator;
+
+import java.util.Map;
+
+public class Finance {
+    public final static String BEST_LOAN_RATES = "bestLoanRates";
+    public final static String SAVINGS_CALCULATOR = "savingsCalculator";
+    public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
+
+    public final static Map<String, String> commandToUsage =
+            Map.of(
+                    BEST_LOAN_RATES, "usage: bestLoanRates",
+                    SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
+                    MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
+            );
+
+    private static boolean validateCommandArguments(String[] args) {
+        switch (args[0]) {
+            case BEST_LOAN_RATES:
+                return args.length == 1;
+
+            case SAVINGS_CALCULATOR:
+                return args.length == 3;
+
+            case MORTGAGE_CALCULATOR:
+                return args.length == 4;
+
+        }
+        return false;
+    }
+
+    /**
+     * This method execute existing command-line apps based on what inputs you receive from the user.
+     */
+    private static void executeCommand(String command, String[] arguments) {
+        switch (command) {
+            case BEST_LOAN_RATES:
+                System.out.println("Finding best loan rates ...");
+                BestLoanRates.main(arguments);
+                return;
+
+            case SAVINGS_CALCULATOR:
+                System.out.println("Finding your net savings ...");
+                SavingsCalculator.main(arguments);
+                return;
+
+            case MORTGAGE_CALCULATOR:
+                System.out.println("Finding your monthly payment ...");
+                MortgageCalculator.main(arguments);
+                return;
+        }
+    }
+}
