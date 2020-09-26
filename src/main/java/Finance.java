97f0c54f4ff268b44bec2caf989199423ad52262
unknown <gatheruteving@gmail.com>
2020-09-27 01:21:48 +0300

    step-6-complete

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -9,7 +9,7 @@ public class Finance {
     public final static String SAVINGS_CALCULATOR = "savingsCalculator";
     public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
-    public final static Map<String, String> commandToUsage =
+    public final static Map<String, String> commandsToUsage =
             Map.of(
                     BEST_LOAN_RATES, "usage: bestLoanRates",
                     SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
@@ -51,5 +51,18 @@ public class Finance {
                 MortgageCalculator.main(arguments);
                 return;
         }
+    }
+
+    public static void main(String[] args) {
+        String command = args[0];
+
+        boolean isValidCommand  = commandsToUsage.containsKey(command);
+
+        if (!isValidCommand) {
+            System.out.println(command + ": command not found");
+            return;
+        }
+
+        executeCommand(command, args);
     }
 }
